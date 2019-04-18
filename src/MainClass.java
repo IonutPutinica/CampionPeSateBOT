263e315d7543e022f7264009387b531cdad976d3
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2019-04-18 13:12:09 +0200

    Declared the entities ArrayList

---
+++ src/MainClass.java
@@ -0,0 +1,8 @@
+// Code by Ionut Putinica
+import java.util.ArrayList;
+public class MainClass {
+	public static void main(String [] args)
+{
+		ArrayList<String> entities = new ArrayList<String>(); // Create an ArrayList object that will have the purpose of storing the fighting entites
+}
+}
