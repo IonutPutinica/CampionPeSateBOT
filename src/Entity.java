c3f68e944f628395d7269e1b1905d3216f2046e4
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2019-04-18 13:32:56 +0200

    Added constructor for Entity

--- src/Entity.java
+++ src/Entity.java
@@ -2,5 +2,11 @@
 public class Entity {
 	private String nameOfEntity;
 	private int numberOfControlledTerritories=1;
+	public Entity(String nameOfEntity, int numberOfControlledTerritories)
+	{
+		this.nameOfEntity=nameOfEntity;
+		this.numberOfControlledTerritories=numberOfControlledTerritories;
+	}
+	
 
 }
