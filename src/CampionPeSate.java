21b5dcb07d5bba9bceff893800e60a19ddf3c9c2
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2019-04-18 13:21:19 +0200

    Added code class + constructor + variables declar

---
+++ src/CampionPeSate.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class CampionPeSate {
+	private String entity;
+	private ArrayList<String> entities;
+	public CampionPeSate(ArrayList entities, String entity)
+	{
+		this.entities=entities;
+		this.entity=entity;
+	}
+}
