e682314a12644ad87df25dede27a406c0e9d3be7
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2019-04-19 09:30:29 +0200

    Added getters and setters for Territory

--- src/Territory.java
+++ src/Territory.java
@@ -1,10 +1,34 @@
 
 public class Territory {
-	private int number;
-	private String name;
-	public Territory(int number, String name)
+	private int territoryNumber;
+	private String territoryName;
+	public Territory(int territoryNumber, String territoryName)
 	{
-		this.number=number;
-		this.name=name;
+		this.territoryNumber=territoryNumber;
+		this.territoryName=territoryName;
+	}
+	
+	//returns the assigned number of a territory
+	public int getTerritoryNumber()
+	{
+		return territoryNumber;
+	}
+	
+	//returns the assigned name of a territory
+	public String getTerritoryName()
+	{
+		return territoryName;
+	}
+	
+	//sets a number for a territory
+	public void setTerritoryNumber(int number)
+	{
+		this.territoryNumber=number;
+	}
+	
+	//sets a name for a territory
+	public void setTerritoryName(String name)
+	{
+		this.territoryName=name;
 	}
 }
